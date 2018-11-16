2ba2f79ab88d0356a6faa6af612eb29b6bcbbba7
857689975 <857689975@qq.com>
2018-11-16 10:15:08 +0800

    模板完成

---
+++ index.js
@@ -0,0 +1,80 @@
+module.exports = {
+	// 模版标识
+	name: 'example',
+	// 模版引擎
+	engine: 'pug',
+
+	// 页面
+	pages: [
+		{
+			name: '首页',
+			// 路径
+			path: '/',
+			// 预览图
+			thumbnail: 'home.png',
+		},
+		{
+			name: '文苑之旅',
+			// 路径
+			path: 'literary',
+			// 预览图
+			thumbnail: 'literary.png',
+		},
+		{
+			name: '景区概况',
+			// 路径
+			path: 'scenic',
+			// 预览图
+			thumbnail: 'scenic.png',
+		},
+		{
+			name: '舒雅住宅',
+			// 路径
+			path: 'residence',
+			// 预览图
+			thumbnail: 'residence.png',
+		},
+		{
+			name: '新闻动态',
+			// 路径
+			path: 'news',
+			// 预览图
+			thumbnail: 'news.png',
+		},
+		{
+			name: '留言中心',
+			// 路径
+			path: 'feedback',
+			// 预览图
+			thumbnail: 'feedback.png',
+		},
+		{
+			name: '联系我们',
+			// 路径
+			path: 'contact',
+			// 预览图
+			thumbnail: 'contact.png',
+		},
+		{
+			name: '文苑之旅详情页',
+			// 路径
+			path: 'literary_show',
+			// 预览图
+			thumbnail: 'literary_show.png',
+		},
+		{
+			name: '新闻详情页',
+			// 路径
+			path: 'news_show',
+			// 预览图
+			thumbnail: 'news_show.png',
+		},
+		{
+			name: '网络地图',
+			// 路径
+			path: 'map',
+			// 预览图
+			thumbnail: 'map.png',
+		}
+	]
+}
